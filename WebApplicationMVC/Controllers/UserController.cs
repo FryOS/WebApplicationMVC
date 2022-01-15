9a331dade1cc8f97f4f1950d8a4d7067431eef53
Alexey Ospishchev <alexeyosp@gmail.com>
2022-01-16 00:13:11 +0300

    Регистрация

---
+++ WebApplicationMVC/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IBlogRepository _repo;
+
+        public UsersController(IBlogRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _repo.GetUsers();
+            return View(authors);
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
